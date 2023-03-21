a113b383da26906d216190d159bbeacd5ae5e55c
ALEXKIM0202 <122650303+ALEXKIM0202@users.noreply.github.com>
2023-03-21 11:27:40 +0700

    Update script.js

--- bjs/08_if_else/script.js
+++ bjs/08_if_else/script.js
@@ -1,5 +1,28 @@
 let minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
 let maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
+
+
+if (minValue !== 0) {
+    defaultMinValue = 0;
+    minValue = minValue || defaultMinValue;
+}
+if (maxValue !== 0) {
+    defaultMaxValue = 100;
+    maxValue = maxValue || defaultMaxValue; 
+}
+
+if (minValue < maxValue) {
+
+  
+    (minValue <= -1000)?
+        minValue = -999:
+        minValue = minValue;
+
+    (maxValue >= 1000)?
+        maxValue = 999:
+        maxValue = maxValue;
+}
+
 alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
 let answerNumber  = Math.floor((minValue + maxValue) / 2);
 let orderNumber = 1;
@@ -91,22 +114,20 @@ document.getElementById('btnOver').addEventListener('click', function () {
 })
 
 
-document.getElementById('btnLess').addEventListener('click', function () {
-    if (gameRun){
-        if (minValue === maxValue){
-            const phraseRandom = Math.round( Math.random());
-            const answerPhrase = (phraseRandom === 1) ?
-                `Вы загадали неправильное число!\n\u{1F914}` :
-                `Я сдаюсь..\n\u{1F92F}`;
-
-            answerField.innerText = answerPhrase;
-            gameRun = false;
-        } else {
-    
-            maxValue = answerNumber ;
-            answerNumber = Math.floor((minValue + maxValue) / 2);
-            orderNumber++;
-            orderNumberField.innerText = orderNumber;
+document.getElementById('btnLess').addEventListener('click', function () { 
+    if (gameRun){  
+        if (minValue === maxValue || answerNumber <= minValue){ 
+            const phraseRandom = Math.round( Math.random()); 
+            const answerPhrase = (phraseRandom === 1) ? 
+                `Вы загадали неправильное число!\n\u{1F914}` : 
+                `Я сдаюсь..\n\u{1F92F}`; 
+            answerField.innerText = answerPhrase; 
+            gameRun = false; 
+        } else { 
+            maxValue = answerNumber; 
+            answerNumber  = Math.floor((minValue + maxValue) / 2); 
+            orderNumber++; 
+            orderNumberField.innerText = orderNumber; 
 
             const phraseRandom1 = Math.round( Math.random() * 3); 
             if (phraseRandom1 === 0) {
@@ -148,3 +169,4 @@ document.getElementById('btnLess').addEventListener('click', function () {
             gameRun = false; 
         }
     })
+
